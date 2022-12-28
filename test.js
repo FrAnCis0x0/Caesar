aa39081140059abd8b9d8d132e769f77703993e6
Francis0x0 <40493292+FrAnCis0x0@users.noreply.github.com>
2022-12-28 22:09:24 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 <script>alert(1)</script>
-
+window.location.hash.substr(1) = <script>alert(1)</script>
+window.alert("sometext");
  scriptEl.onload = function() { 
         setInnerText(document.getElementById("log"),  
           "Loaded gadget from " + url);
