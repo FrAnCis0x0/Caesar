f6f13350e48ef1d80f0a293fe03b8477140a2911
Francis0x0 <40493292+FrAnCis0x0@users.noreply.github.com>
2022-12-28 22:02:51 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 <script>alert(1)</script>
 
+ scriptEl.onload = function() { 
+        setInnerText(document.getElementById("log"),  
+          "Loaded gadget from " + url);
+        <script>alert(1)</script>
+      }
