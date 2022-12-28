b5a6ff57db7a853e07df41d779871c2c91f20f8a
Francis0x0 <40493292+FrAnCis0x0@users.noreply.github.com>
2022-12-29 00:22:51 +1000

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-<script>alert(1)</script>
-window.location.hash.substr(1) = <script>alert(1)</script>
-window.alert("sometext");
- scriptEl.onload = function() { 
-        setInnerText(document.getElementById("log"),  
-          "Loaded gadget from " + url);
-        <script>alert(1)</script>
-      }
